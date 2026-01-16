a716c0fc1024f2d90f11bfeb6c8bc4ba7cd69608
Paco del Olmo <fjdom.pfv.tv@gmail.com>
2026-01-16 17:06:10 +0100

    Feat: Create the backend project

---
+++ WIP_Backend/src/main/java/com/example/wip/WipBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.wip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WipBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WipBackendApplication.class, args);
+	}
+
+}
---
+++ WIP_Backend/src/test/java/com/example/wip/WipBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WipBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
